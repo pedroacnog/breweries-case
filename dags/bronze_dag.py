9861b28ff17da5732ee35032ffbc72cdf96c16d1
João Pedro Gonçalves <joaopedrogoncalves@Joaos-MacBook-Pro.local>
2025-03-30 19:50:29 -0300

    chore: default_args nas dags

--- dags/bronze_dag.py
+++ dags/bronze_dag.py
@@ -2,12 +2,20 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 import pendulum
 
+default_args = {
+    "retries": 3,
+    "retry_delay": pendulum.duration(minutes=5),
+    "email_on_failure": True,
+    "email": ["pedroacnog@gmail.com"],
+}
+
 with DAG(
     "bronze_breweries_ingestion",
     description="API Data Ingestion (Bronze)",
     schedule="0 0 * * *", 
     start_date=pendulum.today('America/Sao_Paulo').add(days=-1),
     catchup=False,
+    default_args=default_args,
     tags=["bronze", "breweries"],
 ) as dag:
 
